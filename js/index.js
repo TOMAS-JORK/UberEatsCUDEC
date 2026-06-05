0306b021b6df96a15a5769d2d0a6fa70b9ee36cb
TOMAS-JORK <hormiguita.7528@gmail.com>
2026-06-04 21:25:14 -0600

    se mostraron los platillos en la aplicacion

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let contenido = '';
 btnAgregarPlatillo = document.getElementById("btnAgregarPlatillo")
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
@@ -11,3 +12,25 @@ document.addEventListener('DOMContentLoaded', function() {
 btnAgregarPlatillo.addEventListener('click', function(){
 alert('Platillo agregado');
 });
+
+function mostrarPlatillo(platillo, id) {
+    contenido += `<div class="card-panel recipe white row" data-id=${id}">
+    <div class="recipe-details">
+      <div class="recipe-title">
+          nombre: ${platillo.nombre}
+      </div>
+      <div class="recipe-ingredients">
+         ingredintes:  ${platillo.ingredientes}
+      </div>
+      <div class="recipe-price">
+         precio $ ${platillo.precio}
+      </div>
+        </div>
+        <div class="recipe-delete">
+        <i class="material-icons" data-id="{id}">delete_outline </i>
+        </div>
+        </div>`;
+        
+
+    document.querySelector('.recipes').innerHTML = contenido;
+}
