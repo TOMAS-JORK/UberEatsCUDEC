395ebd976d0e7d5b06dcf3fa89c1e44d7de23e6d
TOMAS-JORK <hormiguita.7528@gmail.com>
2026-06-25 19:53:10 -0600

    miercoles

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyB8ieRgcdBHqGPOv6EAD6Oc9SeL9jTfiFQ",
+    authDomain: "ubereatscudecrazo.firebaseapp.com",
+    projectId: "ubereatscudecrazo",
+    storageBucket: "ubereatscudecrazo.firebasestorage.app",
+    messagingSenderId: "631379683714",
+    appId: "1:631379683714:web:2809e78de24f74d58d479b"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
